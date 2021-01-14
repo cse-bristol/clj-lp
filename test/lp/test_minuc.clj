fafd3864b338554b1e7c03068e1672ffbf75de07
Tom Hinton <tom.hinton@cse.org.uk>
2021-01-14 09:56:39 +0000

    Add test for minuc in cases where constraints have :and in them

---
+++ test/lp/test_minuc.clj
@@ -0,0 +1,39 @@
+(ns lp.test-minuc
+  (:require  [clojure.test :as t]
+             [lp.scip :as scip]
+             [lp.core :as lp]))
+
+(def insoluble
+  {:minimize [+ :x :y]
+   :vars {:x {} :y {}}
+   :subject-to
+   [^{:name :first} [<= :y :x]
+    ^{:name :second} [>= 5 :x]
+    ^{:name :third} [>= :y 6]
+    ]})
+
+(t/deftest test-insoluble
+  (let [bad-cons (scip/minuc insoluble)]
+    (t/is (not-empty bad-cons))
+    (t/is (#{:first :second :third} (:name (meta (first bad-cons)))))
+    (t/is (= 1 (count bad-cons)))))
+
+(def insoluble-2
+  {:minimize [+ :x :y]
+   :vars {:x {} :y {}}
+   :subject-to
+   [[:and]
+    [:and]
+    [:and
+     ^{:name :first} [<= :y :x]
+     (list)
+
+     ^{:name :second} [>= 5 :x]
+     ^{:name :third} [>= :y 6]
+     ]]})
+
+(t/deftest test-insoluble-2
+  (let [bad-cons (scip/minuc insoluble-2)]
+    (t/is (not-empty bad-cons))
+    (t/is (#{:first :second :third} (:name (meta (first bad-cons)))))
+    (t/is (= 1 (count bad-cons)))))
